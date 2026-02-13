5e3d39921c5917fde2b0e8eca027fa476ffb7129
[a]ster <personal.asterious@gmail.com>
2026-02-13 14:33:49 +0300

    develop | string unquote parse

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -3,6 +3,7 @@ package analyzer
 import (
 	"fmt"
 	"go/ast"
+	"strconv"
 
 	"golang.org/x/tools/go/analysis"
 )
@@ -21,8 +22,24 @@ func run(pass *analysis.Pass) (interface{}, error) {
 				return true
 			}
 
-			testansw := isLog(expr)
-			fmt.Println(testansw, " = islog")
+			if !isLog(expr) {
+				return true
+			}
+
+			arg := expr.Args[0]
+			basicLit, ok := arg.(*ast.BasicLit)
+			if !ok {
+				return true
+			}
+
+			// text := basicLit.Value -- string - выводится с кавычками
+			text, err := strconv.Unquote(basicLit.Value)
+			if err != nil {
+				return true
+			}
+
+			fmt.Println(text)
+			// Checker(text)
 			return true
 		})
 	}
@@ -49,5 +66,18 @@ func isLog(expr *ast.CallExpr) bool {
 	if pack == "slog" || pack == "log" {
 		return true
 	}
+	//TODO: проверку на конкретную функцию, тк там есть log.New() который по факту лог, но текст не выводит
 	return false
+}
+
+func Checker(text string) bool {
+
+	// проверка на большую букву
+	// letters := []rune(text)
+	// if len(letters)>0{
+	// 	firstChar := letters[0]
+	// 	if unicode.IsUpper(firstChar) || firstChar
+	// }
+
+	return true
 }
